1d81b9536febc15edd5980915ec73a0f9073feb9
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2024-01-16 20:08:04 +0200

    create new function get duration

---
+++ datacenter/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils.timezone import localtime
+import datetime
+
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return self.owner_name
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard, on_delete=models.CASCADE)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return '{user} entered at {entered} {leaved}'.format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved=(
+                f'leaved at {self.leaved_at}'
+                if self.leaved_at else 'not leaved'
+            )
+        )
+
+
+def get_duration(visit):
+    local_time = localtime()
+    entered_at = visit.entered_at
+    resides = local_time - entered_at
+    total_duration = resides.total_seconds()
+    return total_duration
+
+
+visit = Visit.objects.all()[0]
+print(get_duration(visit))
